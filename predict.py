ac829bea725b58e59b395f54134689f7863dd94f
Atul Vinod <atulvinod2001@gmail.com>
2023-12-07 21:56:05 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -5,7 +5,10 @@ from preprocess import process_new_data
 def predict_insider_threat(model, features):
     # Prompt user for input
     user_id = input("Enter user ID: ")
+
+    # Prompt for date and check if login attempt is done after hours
     date = input("Enter date (YYYY-MM-DD HH:mm:ss): ")
+    after_hours_attempt = input("Is the login attempt done after hours? (yes/no): ").lower() == 'yes'
 
     # Simulate checking if an external device is connected
     external_device_connected = input("Is an external device connected? (yes/no): ").lower() == 'yes'
@@ -17,13 +20,19 @@ def predict_insider_threat(model, features):
     new_data = pd.DataFrame({
         'user': [user_id],
         'date': [date],
+        'after_hours': [1 if after_hours_attempt else 0],  # Feature engineering for after hours attempt
         'device_count': [1 if external_device_connected else 0],  # Feature engineering for device count
         'attachment_count': [attachment_count],  # Feature engineering for attachment count
         # Add other features based on your requirements
     })
 
+    # Process the new data (if needed)
+    new_data_processed = process_new_data(new_data)
+
+    # Ensure that the features in 'new_data_processed' match the expected 'features'
+    new_features = new_data_processed[features]
+
     # Predict using the model
-    new_features = new_data[features]
     new_predictions = model.predict(new_features)
 
     # Print the prediction
@@ -41,7 +50,7 @@ if __name__ == "__main__":
         trained_model = load('kmeans_model.joblib')  # Update with the correct model file
 
         # Features used for prediction (should match the features used during training)
-        model_features = ['device_count', 'attachment_count']  # Update with your features
+        model_features = ['after_hours', 'device_count', 'attachment_count']  # Update with your features
 
         # Call the prediction function
         predict_insider_threat(trained_model, model_features)
