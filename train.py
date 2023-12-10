4218a2807ae8f9d47a661e1d02551b21b78d331c
Atul Vinod <atulvinod2001@gmail.com>
2023-12-10 21:45:39 +0530

    Update train.py

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ def train_model(X, model_filename='kmeans_model.joblib'):
         # Train a new model if no existing model is found
         kmeans_model = KMeans(n_clusters=2, random_state=42, n_init=10)  # You may need to adjust the number of clusters
         kmeans_model.fit(X)
-        joblib.dump(kmeans_model, model_filename)
+        #joblib.dump(kmeans_model, model_filename)
         print("Trained and saved a new model.")
     
     return kmeans_model
